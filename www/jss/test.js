cec5d47b7eae0f5089d851214b62b81a6679a74b
d.strazel <d.strazel@DAVID-PC.Aplon.local>
2013-05-29 14:30:20 +0200

    xcvcxvcx

---
+++ www/jss/test.js
@@ -0,0 +1,6 @@
+function Calc_TTC(PrixHT, Taux) {
+
+	console.log(parseFloat(PrixHT) + (parseFloat(PrixHT) * parseFloat(Taux) / 100))
+
+	return (parseFloat(PrixHT) + (parseFloat(PrixHT) * parseFloat(Taux) / 100))
+}
